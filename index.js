6ddb1e8abbbf3fb779571f2388c23898dd84381e
Harsh Khandeparkar <34770591+HarshKhandeparkar@users.noreply.github.com>
2019-04-11 19:14:20 +0530

    Improve (#5)
    * Update index.js
    
    * Update index.html
    
    * Update index.css
    
    * Update index.js
    
    * Create jquery-3.4.0.min.js
    
    * Update index.html
    
    * Update index.html
    
    * Update index.js
    
    * Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 document.getElementById("generate").addEventListener("click", genPass, true);
 
 function genPass() {
-  var size = parseInt(document.getElementById("size").value);
+  var size = parseInt($("#size").val());
   
   let checkBoxes = [];
   
-  if (document.getElementById("smallLeters").checked) checkboxes.push('smallLetters');
-  if (document.getElementById("capitalLetters").checked) checkboxes.push('capitalLetters');
-  if (document.getElementById("numbers").checked) checkboxes.push('numbers');
-  if (document.getElementById("specialCharacters").checked) checkboxes.push('specialCharacters');
-  if (document.getElementById("recommended").checked) checkboxes.push('recommended');
-
+  if ($('.checkbox[name="recommended"]').prop('checked')) {
+    $('.checkbox[name!="recommended"]').attr('disabled', 'disabled');
+    checkboxes.push('specialcharacters');
+    checkboxes.push('numbers');
+    checkboxes.push('smallletters');
+    checkboxes.push('capitalletters');
+  }
+  else {
+    $('.checkbox').each(() => {
+      if ($(this).prop('checked')) checkboxes.push($(this).attr('name').toLowerCase());
+    })
+  }
+  
   var output = [];
   
   if (checkBoxes.length < 1) {
@@ -20,9 +27,9 @@ function genPass() {
 
   const charMap = {
     smallletters: [97, 122],
-    capitalLetters: [65, 90],
+    capitalletters: [65, 90],
     numbers: [30, 39],
-    specialChars: [42, 46]
+    specialcharacters: [42, 46]
   }
 
   const getRandom = (max, min=0) => {
@@ -31,10 +38,10 @@ function genPass() {
   
   const getMinMaxFromArr = (arr) => {
     return [
-      arr.reduce((acc, new)) => {
+      arr.reduce((acc, new) => {
         return (acc[0] <= new[0]) ? [acc[0], 0] : [new[0], 0];
       })[0],
-      arr.reduce((acc, new)) => {
+      arr.reduce((acc, new) => {
         return (acc[1] >= new[1]) ? [0, acc[1]] : [0, new[1]];
       })[1]
     ];
